2ef0c56ec3263fe3bd7f668b2d899f7bef7abfc4
Alexey Chekunov <achekunov@mirantis.com>
2016-08-17 17:56:21 +0300

    support fuel 9.0
    Change-Id: I77277731e7c59a94edd23c1bf3455110858afd27

--- deployment_scripts/puppet/modules/lbaas/manifests/params.pp
+++ deployment_scripts/puppet/modules/lbaas/manifests/params.pp
@@ -1,21 +1,10 @@
+#Class lbaas::params
 class lbaas::params {
-  $neutron_conf_file = '/etc/neutron/neutron.conf'
-  $lbaas_conf_file = '/etc/neutron/lbaas_agent.ini'
-  $haproxy_pkg = 'haproxy'
+  $neutron_conf_file         = '/etc/neutron/neutron.conf'
+  $lbaas_conf_file           = '/etc/neutron/lbaas_agent.ini'
+  $lbaas_package_name        = 'neutron-lbaasv2-agent'
+  $lbaas_service_name        = 'neutron-lbaasv2-agent'
+  $lbaas_service_plugin_name = 'neutron_lbaas.services.loadbalancer.plugin.LoadBalancerPluginv2'
+  $lbaas_service_provider    = 'LOADBALANCERV2:Haproxy:neutron_lbaas.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default'
 
-  if $::osfamily == 'Debian' {
-    $httpd_service_name = 'apache2'
-    $horizon_settings_file = '/etc/openstack-dashboard/local_settings.py'
-    $lbaas_package_name = 'neutron-lbaas-agent'
-    $usergroup = 'nogroup'
-
-  } elsif($::osfamily == 'RedHat') {
-    $httpd_service_name = 'httpd'
-    $horizon_settings_file = '/etc/openstack-dashboard/local_settings'
-    $lbaas_package_name = 'openstack-neutron'
-    $usergroup = 'nobody'
-
-  } else {
-    fail("unsupported family ${::osfamily}")
-  }
 }
