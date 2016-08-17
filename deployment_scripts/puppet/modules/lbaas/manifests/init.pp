df3582597ecc08099b8b4cb5a979eaa4318224b8
Alexey Chekunov <achekunov@mirantis.com>
2016-08-17 17:56:21 +0300

    support fuel 9.0
    Change-Id: I77277731e7c59a94edd23c1bf3455110858afd27

--- deployment_scripts/puppet/modules/lbaas/manifests/init.pp
+++ deployment_scripts/puppet/modules/lbaas/manifests/init.pp
@@ -1,40 +1,17 @@
-# == Class: lbaas
+#    Copyright 2016 Mirantis, Inc.
 #
-# Full description of class lbaas here.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# === Parameters
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'lbaas':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 class lbaas {
   include lbaas::params
 
@@ -42,54 +19,49 @@ class lbaas {
     ensure => present,
   }
 
-# Actually, we need to define some resource, where we will do this configs and then we will call this resource from here. It need to better split
-# configuration part (we will try that package with neutron is anstalled in that defined resource) and we can notify something from resource - e.g.
-# we can create notify from that resource to neutron-server service. But now it good in that view.
-# if $::osfamily == 'Debian'{
-#   neutron_config {
-#     "service_providers/service_provider": value => 'LOADBALANCER:Haproxy:neutron.services.loadbalancer.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default';
-#   }
-# }
+  neutron_config {
+    'service_providers/service_provider': value => $lbaas::params::lbaas_service_provider;
+  }
 
-# Sorry, that needs to rework, obviously, but I haven't so much time to do it.
-  exec { "add_lbaas_plugin":
-    command => "/bin/sed -i \"/`egrep -v \'(^#|^$)\' $lbaas::params::neutron_conf_file | egrep \'.*service_plugins.*\'`/ s/$/,lbaas/\" $lbaas::params::neutron_conf_file",
-    unless  => "/bin/egrep -v '(^#|^$)' $lbaas::params::neutron_conf_file | egrep '.*service_plugins.*lbaas.*'",
-    notify  => Service['neutron-server']
+  ini_subsetting {'enable_lbaas_plugin':
+    ensure               => present,
+    section              => 'DEFAULT',
+    key_val_separator    => '=',
+    path                 => $lbaas::params::neutron_conf_file,
+    setting              => 'service_plugins',
+    subsetting           => $lbaas::params::lbaas_service_plugin_name,
+    subsetting_separator => ',',
   }
 
   lbaas_config {
-    "DEFAULT/device_driver": value => 'neutron.services.loadbalancer.drivers.haproxy.namespace_driver.HaproxyNSDriver';
-    "DEFAULT/interface_driver": value => 'neutron.agent.linux.interface.OVSInterfaceDriver';
-    "haproxy/user_group": value => $lbaas::params::usergroup;
+    'DEFAULT/interface_driver': value => 'openvswitch';
   }
 
-# That too should be redesigned, but I'm not sure that I need to write whole new provider to change one string of code.
-  exec { "enable_lbaas":
-    command => "/bin/sed -i \"s/'enable_lb': False/'enable_lb': True/\" $lbaas::params::horizon_settings_file",
-    unless  => "/bin/egrep \"'enable_lb': True\" $lbaas::params::horizon_settings_file",
-    notify  => Service[$lbaas::params::httpd_service_name],
+  exec { 'neutron-db-sync':
+    command   => 'neutron-db-manage --subproject neutron-lbaas upgrade head',
+    path      => '/usr/bin',
+    logoutput => on_failure,
+    require   => Package[$lbaas::params::lbaas_package_name],
   }
+
+  Neutron_config<||> ~> Service<| tag == 'lbaas-service' |>
+  Ini_subsetting<||> ~> Service<| tag == 'lbaas-service' |>
+  Lbaas_config<||>   ~> Service<| tag == 'lbaas-service' |>
 
   service { 'neutron-server':
-    ensure  => running,
-    enable  => true,
-    require => Package[$lbaas::params::lbaas_package_name],
+    ensure     => running,
+    enable     => true,
+    tag        => 'lbaas-service',
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Exec['neutron-db-sync'],
   }
 
-  service { 'neutron-lbaas-agent':
+  service { $lbaas::params::lbaas_service_name:
     ensure  => running,
     enable  => true,
-    require => Package[$lbaas::params::lbaas_package_name],
-  }
-
-  package { $lbaas::params::haproxy_pkg:
-    ensure  => present,
-  }
-
-  service { $lbaas::params::httpd_service_name:
-    enable  => true,
-    ensure  => running,
+    tag     => 'lbaas-service',
+    require => Exec['neutron-db-sync'],
   }
 
 }
